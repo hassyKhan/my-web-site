a84dec21602617b513af55416ebc9393635fca3c
hassykhan <hmunir9191@gmail.com>
2022-12-18 14:11:22 +0500

    this final file

--- palindrome.js
+++ palindrome.js
@@ -1,33 +1,26 @@
 
 function checkPalindrome(string) {
 
-    // find the length of a string
     const len = string.length;
-    if(len<10)
+    if(len>=10)
     {
-    document.getElementById("demo").innerHTML = "Please Enter String of length greater than 10";
+    document.getElementById("demo").innerHTML = "Please Enter String of length less than 10";
     return ;
     }
-    // loop through half of the string
-    for (let i = 0; i < len / 2; i++) {
+     for (let i = 0; i < len / 2; i++) {
     
-        // check if first and last string are same
         if (string[i] !== string[len - 1 - i]) {
             // return 'It is not a palindrome';
-    document.getElementById("demo").innerHTML = "It is not a palindrome";
+    document.getElementById("demo").innerHTML = "not a palindrome";
     return '';
         }
     }
-    document.getElementById("demo").innerHTML = "It is a palindrome";
+    document.getElementById("demo").innerHTML = "palindrome";
     return '';
     }
     
-    // take input
-    
-    // const string = 'madam';
     const string = 'School';
     
-    // call the function
     const value = checkPalindrome(string);
     
     console.log(value);
