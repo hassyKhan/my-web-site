4cb4a71e0065f8bf0029842be0da3abb20dc5822
hassykhan <hmunir9191@gmail.com>
2022-12-18 12:05:22 +0500

    this is final file

---
+++ palindrome.js
@@ -0,0 +1,34 @@
+
+function checkPalindrome(string) {
+
+    // find the length of a string
+    const len = string.length;
+    if(len<10)
+    {
+    document.getElementById("demo").innerHTML = "Please Enter String of length greater than 10";
+    return ;
+    }
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+    
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            // return 'It is not a palindrome';
+    document.getElementById("demo").innerHTML = "It is not a palindrome";
+    return '';
+        }
+    }
+    document.getElementById("demo").innerHTML = "It is a palindrome";
+    return '';
+    }
+    
+    // take input
+    
+    // const string = 'madam';
+    const string = 'School';
+    
+    // call the function
+    const value = checkPalindrome(string);
+    
+    console.log(value);
+    
